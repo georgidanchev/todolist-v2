6f0221d516fdcbf16b431ae8841771609c10019f
Georgi Codes <georgicodes@outlook.com>
2019-03-30 21:38:51 +0000

    some html, style, and logic where added

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import './styles/style.scss'
 
-console.log('hello')
+import Header from './scripts/header'
+
+new Header('.header').onLoad()
---
+++ src/scripts/header.js
@@ -0,0 +1,46 @@
+
+export default class header {
+  constructor(_target) {
+    this.target = document.querySelector(_target)
+    this.body = document.body
+    this.input = this.target.querySelector('[data-input]')
+    this.btnSrch = this.target.querySelector('[data-btn-srch]')
+    this.btnAdd = this.target.querySelector('[data-btn-add]')
+  }
+
+  addTask() {
+    console.log(this.target)
+  }
+
+  submitTask() {
+    console.log(this.target)
+  }
+
+  onLoad() {
+    this.input.addEventListener('click', () => {
+      console.log('input click')
+    })
+
+    // fix this.
+    this.input.addEventListener('change', () => {
+      console.log('keyup')
+    })
+
+    this.input.onkeypress = (e) => {
+      const key = e.charCode || e.keyCode || 0
+
+      if (key === 13) {
+        e.preventDefault()
+        console.log('enter clicked')
+      }
+    }
+
+    this.btnSrch.addEventListener('click', () => {
+      console.log('btn srch')
+    })
+
+    this.btnAdd.addEventListener('click', () => {
+      console.log('btn add')
+    })
+  }
+}
